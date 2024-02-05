fdcacbd4f7d64c7350e6e28d8875d3238045a926
Aman07a <aman16.aa17@gmail.com>
2024-02-05 15:38:46 +0100

    Building an Auth Service

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -4,32 +4,30 @@ import { AbstractControl, AsyncValidator } from '@angular/forms';
 import { map, catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
 
+import { AuthService } from '../auth.service';
+
 @Injectable({
   providedIn: 'root',
 })
 export class UniqueUsername implements AsyncValidator {
-  constructor(private http: HttpClient) {}
+  constructor(private authService: AuthService) {}
 
   validate = (control: AbstractControl) => {
     const { value } = control;
 
-    return this.http
-      .post<any>('https://api.angular-email.com/auth/username', {
-        username: value,
+    return this.authService.usernameAvailable(value).pipe(
+      map((value): any => {
+        if (value.available) {
+          return null;
+        }
+      }),
+      catchError((err) => {
+        if (err.error.username) {
+          return of({ nonUniqueUsername: true });
+        } else {
+          return of({ noConnection: true });
+        }
       })
-      .pipe(
-        map((value): any => {
-          if (value.available) {
-            return null;
-          }
-        }),
-        catchError((err) => {
-          if (err.error.username) {
-            return of({ nonUniqueUsername: true });
-          } else {
-            return of({ noConnection: true });
-          }
-        })
-      );
+    );
   };
 }
