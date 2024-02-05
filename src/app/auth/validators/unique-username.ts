83516acacb63aa873973c5bf6a69a418da666f3e
Aman07a <aman16.aa17@gmail.com>
2024-02-05 14:23:15 +0100

    Nasty Async Validators

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { AsyncValidator } from '@angular/forms';
+import { AsyncValidator, AbstractControl } from '@angular/forms';
 
 @Injectable({ providedIn: 'root' })
 export class UniqueUsername implements AsyncValidator {
   constructor(private http: HttpClient) {}
 
-  validate() {}
+  validate(control: AbstractControl): any {
+    const { value } = control;
+
+    console.log(this.http);
+
+    return null;
+  }
 }
