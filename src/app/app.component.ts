a177cd7c6bb6da6d23df28ae386c5d229e127fb6
Aman07a <aman16.aa17@gmail.com>
2024-02-07 12:45:35 +0100

    The Async Pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'emailclient';
-  signedin = false;
+  signedin$: BehaviorSubject<boolean>;
 
-  constructor(private authService: AuthService) {}
-
-  ngOnInit() {
-    this.authService.signedin$.subscribe((signedin) => {
-      this.signedin = signedin;
-    });
+  constructor(private authService: AuthService) {
+    this.signedin$ = this.authService.signedin$;
   }
 }
