8992b273ddb93d4f30d782e259222cabfaf46809
Aman07a <aman16.aa17@gmail.com>
2024-02-15 11:23:32 +0100

    Canceling Previous Email Requests

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EmailService } from '../email.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-email-show',
@@ -14,12 +15,20 @@ export class EmailShowComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    console.log(this.route);
+    // this.route.params.subscribe(({ id }) => {
+    //   this.emailService.getEmail(id).subscribe((email) => {
+    //     console.log(email);
+    //   });
+    // });
 
-    this.route.params.subscribe(({ id }) => {
-      this.emailService.getEmail(id).subscribe((email) => {
+    this.route.params
+      .pipe(
+        switchMap(({ id }) => {
+          return this.emailService.getEmail(id);
+        })
+      )
+      .subscribe((email) => {
         console.log(email);
       });
-    });
   }
 }
