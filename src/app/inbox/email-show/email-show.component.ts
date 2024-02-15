cf787f8361caf1df70e3c7d1d925febd81e5a588
Aman07a <aman16.aa17@gmail.com>
2024-02-15 11:17:50 +0100

    Issues with Nested Subscribes

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-show',
@@ -7,13 +8,18 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./email-show.component.css'],
 })
 export class EmailShowComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private emailService: EmailService
+  ) {}
 
   ngOnInit() {
     console.log(this.route);
 
-    this.route.params.subscribe((value) => {
-      console.log(value);
+    this.route.params.subscribe(({ id }) => {
+      this.emailService.getEmail(id).subscribe((email) => {
+        console.log(email);
+      });
     });
   }
 }
