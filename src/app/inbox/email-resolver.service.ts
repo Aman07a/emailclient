bc5b62e2567c3062a7d140709bfec8187ed49311
Aman07a <aman16.aa17@gmail.com>
2024-02-15 15:13:54 +0100

    Reusable Data Fetching with Resolvers

---
+++ src/app/inbox/email-resolver.service.ts
@@ -0,0 +1,8 @@
+import { ResolveFn } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { Email } from './email';
+
+const emailResolverService: ResolveFn<any> = () => {
+  return undefined;
+};
