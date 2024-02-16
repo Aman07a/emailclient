9f6f77f9b48a23599121356f48af8511f3a08c63
Aman07a <aman16.aa17@gmail.com>
2024-02-16 11:12:51 +0100

    Toggling Modal Visibility

--- src/app/inbox/email-create/email-create.component.ts
+++ src/app/inbox/email-create/email-create.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-email-create',
   templateUrl: './email-create.component.html',
   styleUrls: ['./email-create.component.css'],
 })
-export class EmailCreateComponent {}
+export class EmailCreateComponent implements OnInit {
+  showModal = false;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
