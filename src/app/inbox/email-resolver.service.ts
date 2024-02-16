8fbf4a5abd19b92557e00f7b6d415729631620dd
Aman07a <aman16.aa17@gmail.com>
2024-02-16 09:19:02 +0100

    Error Handling with Resolvers

--- src/app/inbox/email-resolver.service.ts
+++ src/app/inbox/email-resolver.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ResolveFn, ActivatedRouteSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import {
+  ResolveFn,
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { Email } from './email';
 import { EmailService } from './email.service';
@@ -9,12 +15,19 @@ import { EmailService } from './email.service';
   providedIn: 'root',
 })
 export class EmailResolverService {
-  constructor(private emailService: EmailService) {}
+  constructor(private emailService: EmailService, private router: Router) {}
 
   resolve: ResolveFn<Email | any> = (
-    route: ActivatedRouteSnapshot
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
   ): Observable<Email> | Promise<Email> | Email => {
     const emailId = route.params['id'];
-    return this.emailService.getEmail(emailId);
+    return this.emailService.getEmail(emailId).pipe(
+      catchError(() => {
+        this.router.navigateByUrl('/inbox/not-found');
+
+        return EMPTY;
+      })
+    );
   };
 }
